a6173ef17685cd8a0abc45353c024029a8c58f19
Manishbhargav12 <bhargavmanish592@gmail.com>
2026-01-30 18:41:15 +0530

    Delete Python/data analysis/Car/ev directory

--- Python/data analysis/Car/ev/ev.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-data = pd.read_csv("ev.csv")
-df=data.copy()
-# dominting region in 2023 for ev selling
-top_region=df["Region"].value_counts()
-dt=df["Date"].value_counts()
-print(dt)
